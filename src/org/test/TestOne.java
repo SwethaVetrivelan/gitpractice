380f35f7f84b20cfadc350e4d2158dd68cc3b395
SwethaVetrivelan <swetha5vsm@gmail.com>
2021-12-07 20:53:34 +0530

    Swethatask

---
+++ src/org/test/TestOne.java
@@ -0,0 +1,18 @@
+package org.test;
+
+public class TestOne extends TestTwo{
+	
+	public void testMethod() {
+		// TODO Auto-generated method stub
+methodOne();
+methodTwo();
+}
+	public static void main(String[] args) {
+	System.out.println("vanthutanu sollu");
+	TestOne obj= new TestOne();
+	//obj.methodOne();
+
+		obj.testMethod();
+	}
+
+}
---
+++ src/org/test/TestTwo.java
@@ -0,0 +1,13 @@
+package org.test;
+
+public class TestTwo {
+public void methodOne() {
+	// TODO Auto-generated method stub
+	
+System.out.println("Thirumba vanthutanun sollu");
+}
+public void methodTwo() {
+	// TODO Auto-generated method stub
+methodOne();
+}
+}
