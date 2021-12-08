d57c7d08b360b6767e46a4d9caa1cd87a00191b0
SwethaVetrivelan <swetha5vsm@gmail.com>
2021-12-08 13:55:38 +0530

    Swetha1

--- src/org/test/TestTwo.java
+++ src/org/test/TestTwo.java
@@ -1,13 +1,13 @@
 package org.test;
 
 public class TestTwo {
+	public void methodTwo() {
+		// TODO Auto-generated method stub
+	methodOne();
+	}
 public void methodOne() {
 	// TODO Auto-generated method stub
 	
 System.out.println("Thirumba vanthutanun sollu");
-}
-public void methodTwo() {
-	// TODO Auto-generated method stub
-methodOne();
 }
 }
